496ccf257f036f70e7db177d9e146dc4b4560685
Andre Dixon <dredix84@gmail.com>
2017-02-06 14:41:26 -0500

    Revert "First logic commit"
    This reverts commit 95503e629f27da742790ffa1911ee9a233bb945c.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,32 +5,17 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
-class DefaultController extends Controller {
-
+class DefaultController extends Controller
+{
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request) {
-        $serializer = $this->get('serializer');
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-                    'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
-
-    /**
-     * @Route("/admin")
-     */
-    public function adminAction() {
-        $serializer = $this->get('serializer');
-        $response = new JsonResponse();
-        $response->setData(array(
-            'data' => 123
-        ));
-        return $response;
-        //return new Response('<html><body>Admin page!</body></html>');
-    }
-
 }
